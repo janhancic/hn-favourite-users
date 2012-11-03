ba35e9929f8ca73eab2499da228745b0623810f4
Jan Hancic <jan@popcom.si>
2012-11-03 12:03:45 +0100

    find users on a page and change link style

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-alert ( 'Hi!' );
+// utils
+var forEach = Array.prototype.forEach;
+
+// tmp static users list
+var usersToHighlight = [
+	'pg',
+	'CWIZO',
+	'6ren',
+	'rishi'
+];
+
+// get all user links on the page
+var userLinks = document.querySelectorAll ( 'a[href^="user?id="]' );
+
+var usersFound = [];
+forEach.call ( userLinks, function ( userLink ) {
+	if ( usersToHighlight.indexOf ( userLink.innerHTML ) !== -1 ) {
+		console.log ( userLink.href );
+		userLink.style.color = 'white';
+		userLink.style.backgroundColor = '#ff6600';
+
+		usersFound.push ( userLink.innerHTML );
+	}
+} );
+
+console.log ( usersFound );
