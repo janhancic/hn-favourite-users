3dcf7dba62330969e6e5cf33275d001232fcf19d
Jan Hancic <jan@popcom.si>
2012-11-07 21:35:20 +0100

    add options link

--- script.js
+++ script.js
@@ -7,46 +7,70 @@ chrome.extension.sendRequest ( { method: 'getData' }, function ( extensionData )
 	var highlightBackgroundColour = extensionData.highlightBackgroundColour;
 	var highlightColour = extensionData.highlightColour;
 
-	// get all user links on the page
-	var userLinks = document.querySelectorAll ( 'a[href^="user?id="]' );
-
-	var usersFound = [];
-	forEach.call ( userLinks, function ( userLink ) {
-		if ( userLink.parentNode && userLink.parentNode.className === 'pagetop' ) {
-			return;
-		}
-
-		if ( usersToHighlight.indexOf ( userLink.innerHTML ) !== -1 ) {
-			userLink.style.color = highlightColour;
-			userLink.style.backgroundColor = highlightBackgroundColour;
-			userLink.className = 'HnFU-User';
-
-			if ( usersFound.indexOf ( userLink.innerHTML ) === -1 ) {
-				usersFound.push ( userLink.innerHTML );
-			}
-		}
-	} );
-
 	var foundUsersEl = document.createElement ( 'div' );
 	foundUsersEl.id = 'HnFU-FoundUsers';
 
-	if ( usersFound.length > 0 ) {
-		var foundUsersTitleEl = document.createElement ( 'h1' );
-		foundUsersTitleEl.innerHTML = 'Your favourite users:';
-		foundUsersEl.appendChild ( foundUsersTitleEl );
+	if ( location.href.indexOf ( 'user?id=' ) !== -1 ) {
+		var currentUser = location.href.substr ( location.href.indexOf ( 'user?id=' ) + 8 );
 
-		usersFound.forEach ( function ( userFound ) {
-			var tmpEl = document.createElement ( 'a' );
-			tmpEl.href = 'http://news.ycombinator.com/user?id=' + userFound;
-			tmpEl.innerHTML = userFound;
-
-			foundUsersEl.appendChild ( tmpEl );
-		} );
+		if ( usersToHighlight.indexOf ( currentUser ) !== -1 ) {
+			var isFavouriteEl = document.createElement ( 'div' );
+			isFavouriteEl.innerHTML = currentUser + ' is your favourite user';
+			foundUsersEl.appendChild ( isFavouriteEl );
+		} else {
+			var isNotFavouriteEl = document.createElement ( 'div' );
+			isNotFavouriteEl.innerHTML = currentUser + ' is not on your favourite users list';
+			foundUsersEl.appendChild ( isNotFavouriteEl );
+		}
 	} else {
-		var noUsersFoundEl = document.createElement ( 'div' );
-		noUsersFoundEl.innerHTML = 'No favourite users found.<br /><a href="' + chrome.extension.getURL ( 'options.html' ) + '">Click here</a>, to add some users<br /> to your favourites list.';
-		foundUsersEl.appendChild ( noUsersFoundEl );
+		// get all user links on the page
+		var userLinks = document.querySelectorAll ( 'a[href^="user?id="]' );
+
+		var usersFound = [];
+		forEach.call ( userLinks, function ( userLink ) {
+			if ( userLink.parentNode && userLink.parentNode.className === 'pagetop' ) {
+				return;
+			}
+
+			if ( usersToHighlight.indexOf ( userLink.innerHTML ) !== -1 ) {
+				userLink.style.color = highlightColour;
+				userLink.style.backgroundColor = highlightBackgroundColour;
+				userLink.className = 'HnFU-User';
+
+				if ( usersFound.indexOf ( userLink.innerHTML ) === -1 ) {
+					usersFound.push ( userLink.innerHTML );
+				}
+			}
+		} );
+
+		if ( usersFound.length > 0 ) {
+			var foundUsersTitleEl = document.createElement ( 'h1' );
+			foundUsersTitleEl.innerHTML = 'Your favourite users:';
+			foundUsersEl.appendChild ( foundUsersTitleEl );
+
+			usersFound.forEach ( function ( userFound ) {
+				var tmpEl = document.createElement ( 'a' );
+				tmpEl.href = 'http://news.ycombinator.com/user?id=' + userFound;
+				tmpEl.innerHTML = userFound;
+
+				foundUsersEl.appendChild ( tmpEl );
+			} );
+		} else {
+			var noUsersFoundEl = document.createElement ( 'div' );
+			noUsersFoundEl.innerHTML = 'No favourite users found.';
+			foundUsersEl.appendChild ( noUsersFoundEl );
+		}
 	}
+
+	var optionsContainerEl = document.createElement ( 'div' );
+	optionsContainerEl.id = 'HnFU-OptionsLinkContainer';
+	foundUsersEl.appendChild ( optionsContainerEl );
+
+	var optionsLink = document.createElement ( 'a' );
+	optionsLink.href = chrome.extension.getURL ( 'options.html' );
+	optionsLink.title = 'HN Favourite Users - options';
+	optionsLink.id = 'HnFU-OptionsLink';
+	foundUsersEl.appendChild ( optionsLink );
 
 	document.body.appendChild ( foundUsersEl );
 } );
